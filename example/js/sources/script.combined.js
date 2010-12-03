eb7456529d3a883f0f153d5b69d213172c0aa02b
Vincent Voyer <vincent@zeroload.net>
2010-12-03 10:44:08 +0100

    test page example concours

---
+++ example/js/sources/script.combined.js
@@ -0,0 +1,5 @@
+<!--#include file="jquery-1.4.4.js" -->
+<!--#include file="f_market.js" -->
+<!--#include file="package.js" -->
+<!--#include file="master.js" -->
+<!--#include file="f_body.js" -->
